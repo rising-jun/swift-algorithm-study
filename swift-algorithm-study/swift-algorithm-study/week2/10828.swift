7cc63b65a85ec421432c69d6b68681d0f40a7e87
kimdongjune <jun36817@naver.com>
2022-02-13 17:12:42 +0900

    idx를 이용한 스택 구현. 완료

--- swift-algorithm-study/swift-algorithm-study/week2/10828.swift
+++ swift-algorithm-study/swift-algorithm-study/week2/10828.swift
@@ -72,3 +72,36 @@ struct MyStack{
     
 }
 
+struct MyStack2{
+    public private(set) var arr: [String] = []
+    public private(set) var idx: Int = -1
+    public mutating func push(_ val: String){
+        self.arr.append(val)
+        idx += 1
+    }
+    
+    public func top() -> String{
+        if idx == 0{
+            return "-1"
+        }
+        return arr[idx - 1]
+    }
+    
+    public func size() -> Int{
+        return idx + 1
+    }
+    
+    public func empty() -> Int{
+        return size()  == 0 ? 1 : 0
+    }
+    
+    public mutating func pop() -> String{
+        if size() == 0{
+            return "-1"
+        }
+        return self.arr.removeLast()
+    }
+    
+    
+    
+}
